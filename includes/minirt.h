e0a680ecacc7304ba73f56cdaffe148d0b59b2ea
Suzan Underleaf <sunderle@ox-p3.msk.21-school.ru>
2021-03-25 22:25:26 +0300

    init

---
+++ includes/minirt.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 22:24:28 by sunderle          #+#    #+#             */
+/*   Updated: 2021/03/25 22:24:29 by sunderle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+typedef struct	s_win
+{
+	void		*mlx;
+	void		*win;
+	void		*img;
+	void		*addr;
+	int			line_l;
+	int			bpp;
+	int			en;
+}				t_win;
+
+#endif
