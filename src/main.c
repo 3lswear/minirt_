6eeaac3a1f4508b5ae6e2e42dd8202521126795f
3lswear <r.malyavsky@protonmail.com>
2021-04-18 01:32:53 +0300

    now can draw a circle!!!!!!!!!!!!!!!!!

--- src/main.c
+++ src/main.c
@@ -6,19 +6,58 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 22:23:19 by sunderle          #+#    #+#             */
-/*   Updated: 2021/03/25 22:23:22 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/04/18 01:32:43 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../mlx/mlx.h"
 #include "../includes/minirt.h"
+
+void            pixel_put(t_win *win, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = win->addr + (y * win->line_l + x * (win->bpp / 8));
+    *(unsigned int*)dst = color;
+}
 
 int	main(void)
 {
+	int width;
+	int height;
+	/* int offset; */
+
+	width = 1366;
+	height = 768;
+
 	t_win window;
 
     window.mlx = mlx_init();
-    window.win = mlx_new_window(window.mlx, 1366, 768, "jopa");
+    window.win = mlx_new_window(window.mlx, width, height, "jopa");
+	
+	window.img = mlx_new_image(window.mlx, width, height);
+	window.addr = mlx_get_data_addr(window.img, &window.bpp, &window.line_l, 
+			&window.en);
+
+
+	int x;
+	int y;
+
+	x = -width / 2;
+	while (x < width / 2)
+	{
+		y = -height / 2;
+		while (y < height / 2)
+		{
+			if (pow(x, 2) + pow(y, 2) <= 22500)
+				pixel_put(&window, x + width / 2, y + height / 2, BLUE);
+			/* mlx_pixel_put(window.mlx, window.win, x, y, 0x000000FF); */
+			y++;
+		}
+		x++;
+	}
+
+	mlx_put_image_to_window(window.mlx, window.win, window.img, 0, 0);
+
     mlx_loop(window.mlx);
 	return (0);
 }      
