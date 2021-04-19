2d189973d7a4e43f9880ecf15219869f34458505
3lswear <r.malyavsky@protonmail.com>
2021-04-20 02:48:16 +0300

    added descriptive error messages

--- src/utils/handle_error.c
+++ src/utils/handle_error.c
@@ -1,11 +1,22 @@
 #include "minirt.h"
 
-void handle_error(int value)
+void handle_error(int code)
 {
-	if (1)
-	{
-		printf("[x] Error code: %d\n", value);
-		printf("uh oh, something bad happened!!!!!!!!\n");
-	}
-	exit(value);
+	char *msg;
+
+	msg = NULL;
+	if (code == ERR_ALLOC)
+		msg = "Allocation error!";
+	else if (code == ERR_LIST)
+		msg = "List error";
+	else
+		msg = "idk 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔";
+
+	ft_putstr_fd("😰😰😰😰 uh oh, something bad happened!!!!!!!! 😰😰😰😰\n", 2);
+	ft_putstr_fd("[x] Error code => ", 2);
+	ft_putnbr_fd(code, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(msg, 2);
+	
+	exit(code);
 }
