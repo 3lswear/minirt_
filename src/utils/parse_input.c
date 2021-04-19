9be5433da40957d01b7bf57e8be0b61baa509161
3lswear <r.malyavsky@protonmail.com>
2021-04-20 02:48:36 +0300

    tested split_ws function

--- src/utils/parse_input.c
+++ src/utils/parse_input.c
@@ -1,21 +1,61 @@
 #include "minirt.h"
 
-void parse_input(char *file)
+void ft_putstr(void *str)
+{
+	ft_putstr_fd((char *)str, 1);
+}
+
+void lst_print(t_list *lst)
+{
+	while (lst)
+	{
+		ft_putstr_fd(lst->data, 1);
+		ft_putstr_fd(" -> ", 1);
+		lst = lst->next;
+	}
+	ft_putendl_fd("", 1);
+
+}
+
+void parse_input(char *file, t_scene *scene)
 {
 	int fd;
 	char *line;
 	int ret;
+	char **data;
 	fd = open(file, O_RDONLY);
+	(void)scene;
 
 	while ((ret = get_next_line(fd, &line)) == 1)
 	{
-		/* printf("the line is =>[%s]\n", line); */
-		printf("%s\n", line);
+		printf("line from gnl => %s\n", line);
+		data = split_ws(line);
+		int splt_i = 0;
+		while (data[splt_i])
+		{
+			printf("[%s]\n", data[splt_i]);
+			splt_i++;
+		}
+
 		free(line);
 	}
 	if (!ret)
 		free(line);
-	printf("gnl returned -> [%d]\n", ret);
+
+
+	printf("testing lists!!!!!\n");
+
+	/* t_list *pog =  ft_lstnew("pogchamp"); */
+	/* t_list *pepega =  ft_lstnew("pepega"); */
+	/* t_list *lul =  ft_lstnew("lul"); */
+	/* ft_lstadd_back(&pog, pepega); */
+	/* ft_lstadd_back(&pog, lul); */
+
+	/* lst_print(pog); */
+	/* handle_error(ERR_ALLOC); */
+
+	
+	/* printf("gnl returned -> [%d]\n", ret); */
 
 	close(fd);
 }
