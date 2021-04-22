2a5304a317fdca36447106da9b24fac268a6e044
3lswear <r.malyavsky@protonmail.com>
2021-04-22 16:06:29 +0300

    added color overflow check, added color sum func

--- includes/camera.h
+++ includes/camera.h
@@ -10,6 +10,7 @@ typedef struct	s_cam
 }				t_cam;
 
 t_cam *new_cam(t_vec *origin, t_vec *dir, float fov);
-int argb_color(unsigned char r, unsigned char g, unsigned char b);
+int argb_color(int r, int g, int b);
+int c_add(int color1, int color2);
 
 #endif
--- src/inter/inter_sphere.c
+++ src/inter/inter_sphere.c
@@ -1,17 +1,45 @@
 #include "minirt.h"
 
-int argb_color(unsigned char r, unsigned char g, unsigned char b)
+int argb_color(int r, int g, int b)
 {
 	int color;
+
+	if (r > 255)
+		r = 255;
+	if (g > 255)
+		g = 255;
+	if (b > 255)
+		b = 255;
 	color = 0;
 	color += r;
 	color = color << 8;
 	color += g;
 	color = color << 8;
-	color+= b;
+	color += b;
 	/* printf("color is %X\n", color); */
 	return (color);
+}
 
+int c_add(int color1, int color2)
+{
+	int result;
+	int channel;
+	/* 0xAARRGGBB */
+
+	channel = (color1 & 0x00FF0000) + (color2 & 0x00FF0000);
+	if (channel > 0x00FF0000)
+		channel = 0x00FF0000;
+	result = channel;
+	channel = (color1 & 0x0000FF00) + (color2 & 0x0000FF00);
+	if (channel > 0x0000FF00)
+		channel = 0x0000FF00;
+	result += channel;
+	channel = (color1 & 0x000000FF) + (color2 & 0x000000FF);
+	if (channel > 0x000000FF)
+		channel = 0x000000FF;
+	result += channel;
+
+	return (result);
 }
 
 int inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
@@ -24,7 +52,7 @@ int inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 	while (current)
 	{
 		if (inter_sphere(cam, ray, current->data))
-			color = ((t_sphere *)(current->data))->color;
+			color = c_add(((t_sphere *)(current->data))->color, scene->ambient);
 		current = current->next;
 	}
 	return (color);
