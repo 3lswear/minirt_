58948a15b685830697d3e65019d662b3e3fe6523
3lswear <r.malyavsky@protonmail.com>
2021-04-23 02:21:31 +0300

    ADDED LIGHT SOURCE SUPPORT, also added rgb_masks to c_add, added c_mul, modified inter_sphere to return closest point

---
+++ src/geom/lightsource.c
@@ -0,0 +1,13 @@
+#include "minirt.h"
+
+t_light *l_new(t_vec *coords, float brightess, int color)
+{
+	t_light *light;
+	light = ft_calloc(sizeof(t_light), 1);
+	if (!light)
+		handle_error(-1);
+	light->coords = coords;
+	light->brightess = brightess;
+	light->color = color;
+	return (light);
+}
--- src/inter/inter_sphere.c
+++ src/inter/inter_sphere.c
@@ -26,39 +26,92 @@ int c_add(int color1, int color2)
 	int channel;
 	/* 0xAARRGGBB */
 
-	channel = (color1 & 0x00FF0000) + (color2 & 0x00FF0000);
-	if (channel > 0x00FF0000)
-		channel = 0x00FF0000;
+	channel = (color1 & R_MASK) + (color2 & R_MASK);
+	if (channel > R_MASK)
+		channel = R_MASK;
 	result = channel;
-	channel = (color1 & 0x0000FF00) + (color2 & 0x0000FF00);
-	if (channel > 0x0000FF00)
-		channel = 0x0000FF00;
+	channel = (color1 & G_MASK) + (color2 & G_MASK);
+	if (channel > G_MASK)
+		channel = G_MASK;
 	result += channel;
-	channel = (color1 & 0x000000FF) + (color2 & 0x000000FF);
-	if (channel > 0x000000FF)
-		channel = 0x000000FF;
+	channel = (color1 & B_MASK) + (color2 & B_MASK);
+	if (channel > B_MASK)
+		channel = B_MASK;
 	result += channel;
 
 	return (result);
+}
+
+int c_mul(int color, float mul)
+{
+	int result;
+
+	if (mul > 1)
+		return (color);
+	result = (color >> 16) * mul;
+	result = result << 8;
+	result += ((color & G_MASK) >> 8) * mul;
+	result = result << 8;
+	result += (color & B_MASK) * mul;
+	return (result);
+}
+
+int calc_light(t_sphere *sphere, t_light *light, t_vec *ray, float ray_len)
+{
+	t_point *surface_point;
+	t_vec *n;
+	t_vec *l;
+	float mult;
+
+
+	surface_point = v_mult(ray, ray_len);
+	l = v_sub(light->coords, surface_point);
+	n = v_sub(surface_point, sphere->center);
+	v_norm(n);
+	/* printf("surface_point\t-> %f,%f,%f\n", surface_point->x, surface_point->y, surface_point->z); */
+	/* printf("light_point\t-> %f,%f,%f\n", l_norm->x, l_norm->y, l_norm->z); */
+	mult = v_dot_product(n, l) / (v_len(l) * v_len(n));
+	/* printf("mult => %f\n", mult); */
+	/* printf("color =>%X\n", color); */
+	if (mult > 0)
+		return(c_mul(light->color, (light->brightess * mult)));
+	else
+		return (0);
 }
 
 int inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 {
 	int color;
 	t_list *current;
+	t_sphere *sphere;
+	float ray_len;
+	/* float ray_len_new; */
 
 	color = -1;
+	ray_len = 0;
 	current = scene->spheres;
 	while (current)
 	{
-		if (inter_sphere(cam, ray, current->data))
-			color = c_add(((t_sphere *)(current->data))->color, scene->ambient);
+		sphere = current->data;
+		/* if (ray_len_new < ray_len) */
+
+		ray_len = inter_sphere(cam->origin, ray, current->data);
+		if (ray_len > 0)
+		{
+			color = c_add(sphere->color, scene->ambient);
+			if (scene->light)
+				color = c_add(color, calc_light(sphere, scene->light, ray, ray_len));
+			if (color > 0)
+			{
+				return (color);
+			}
+		}
 		current = current->next;
 	}
 	return (color);
 }
 
-int inter_sphere(t_cam *cam, t_vec *ray, t_sphere *sphere)
+float inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere)
 {
 	float b;
 	float c;
@@ -71,7 +124,7 @@ int inter_sphere(t_cam *cam, t_vec *ray, t_sphere *sphere)
 	hit2 = 0;
 
 	/* printf("sphere->radius = %f\n", sphere->radius); */
-	cam2sphere = v_sub(cam->origin, sphere->center);
+	cam2sphere = v_sub(origin, sphere->center);
 	b = 2 * v_dot_product(cam2sphere, ray);
 	c = v_dot_product(cam2sphere, cam2sphere) - (pow(sphere->radius, 2));
 	discr = (b * b) - (4 * c);
@@ -82,8 +135,21 @@ int inter_sphere(t_cam *cam, t_vec *ray, t_sphere *sphere)
 	hit2 = (-b + sqrt(discr)) / 2;
 	//choose which distance to return
 	/* if ((hit1 * hit2) > 0) */
-	if (hit1 > 0)
-		return (1);
+	/* ОПТИМИЗИРОВАТЬ */
+	if (hit1 > 0 && hit2 > 0)
+	{
+		if (hit1 > hit2)
+			return (hit2);
+		else
+			return (hit1);
+	}
+	else if ((hit1 * hit2) < 0)
+	{
+		if (hit1 > 0)
+			return (hit1);
+		else
+			return (hit2);
+	}
 	else
 		return (0);
 }
