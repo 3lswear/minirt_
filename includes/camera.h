01f86f6d2fb0f00f590087cfbc395239fe7a7df1
3lswear <r.malyavsky@protonmail.com>
2021-04-24 00:40:22 +0300

    changed types to t_color

--- includes/camera.h
+++ includes/camera.h
@@ -10,7 +10,8 @@ typedef struct	s_cam
 }				t_cam;
 
 t_cam *new_cam(t_vec *origin, t_vec *dir, float fov);
-int argb_color(int r, int g, int b);
-int c_add(int color1, int color2);
+t_color argb_color(int r, int g, int b);
+t_color c_add(t_color color1, t_color color2);
+t_color c_mul(t_color color, float mul);
 
 #endif
