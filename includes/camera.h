3655d30d326302325ad2b09b57fa255ab9e7c9dc
3lswear <r.malyavsky@protonmail.com>
2021-04-24 15:16:56 +0300

    added c_mul to multiply 2 colors

--- includes/camera.h
+++ includes/camera.h
@@ -11,7 +11,9 @@ typedef struct	s_cam
 
 t_cam *new_cam(t_vec *origin, t_vec *dir, float fov);
 t_color argb_color(int r, int g, int b);
+/* int argb_color(t_color color); */
 t_color c_add(t_color color1, t_color color2);
-t_color c_mul(t_color color, float mul);
+t_color c_mul_scalar(t_color color, float mul);
+t_color c_mul(t_color color1, t_color color2);
 
 #endif
