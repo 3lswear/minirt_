5cb668e1848eeed81e5d8b97917c51515cc744c4
3lswear <r.malyavsky@protonmail.com>
2021-04-24 15:18:06 +0300

    chaned to t_color

--- includes/inter.h
+++ includes/inter.h
@@ -2,6 +2,6 @@
 # define INTER_H
 
 
-int inter_objects(t_cam *cam, t_vec *ray, t_scene *scene);
+t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene);
 float inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere);
 #endif
