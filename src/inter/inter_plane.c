50b367a671d5ad4b6d9c8c978c02ab726514eaa7
3lswear <r.malyavsky@protonmail.com>
2021-04-25 18:48:25 +0300

    init

---
+++ src/inter/inter_plane.c
@@ -0,0 +1,57 @@
+#include "minirt.h"
+
+float inter_plane(t_vec *origin, t_vec *ray, t_plane *plane)
+{
+	float ray_len;
+	float t;
+
+	ray_len = 0;
+	(void)origin;
+	/* if ((n->x * (ray->x - m->x) + */
+	/* 		n->y * (ray->y - m->y) + */
+	/* 		n->z * (ray->z - m->z)) == 0) */
+	/* 	return () */
+	t = (v_dot_product(v_sub(plane->pos, origin), plane->norm))
+			/ (v_dot_product(ray, plane->norm));
+	/* ray_len = v_mult(ray, t); */
+	ray_len = t;
+	/* printf("len to plane => %f\n", t); */
+	if (t > 0)
+	{
+		return (t);
+	}
+	else
+		return (0);
+}
+
+/* t_color calc_light_matte_plane(t_plane *sphere, t_light *light, t_vec *ray, float ray_len) */
+/* { */
+/* 	t_point *surface_point; */
+/* 	t_vec *n; */
+/* 	t_vec *l; */
+/* 	float strength; */
+
+
+/* 	surface_point = v_mult(ray, ray_len); */
+/* 	l = v_sub(light->coords, surface_point); */
+/* 	n = v_sub(surface_point, sphere->center); */
+/* 	v_norm(n); */
+/* 	/1* printf("surface_point\t-> %f,%f,%f\n", surface_point->x, surface_point->y, surface_point->z); *1/ */
+/* 	/1* printf("light_point\t-> %f,%f,%f\n", l_norm->x, l_norm->y, l_norm->z); *1/ */
+/* 	strength = v_dot_product(n, l) / (v_len(l) * v_len(n)); */
+/* 	/1* printf("strength => %f\n", strength); *1/ */
+/* 	/1* printf("color =>%X\n", color); *1/ */
+/* 	free(surface_point); */
+/* 	if (v_dot_product(n, l) > 0) */
+/* 	{ */
+/* 		free(l); */
+/* 		free(n); */
+/* 		return(c_mul_scalar(light->color, (light->brightess * strength))); */
+/* 	} */
+/* 	else */
+/* 	{ */
+/* 		free(l); */
+/* 		free(n); */
+/* 		return (0); */
+/* 	} */
+/* } */
