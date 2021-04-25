3486982aecb0eff51e4a3f418f2710b5db65f985
3lswear <r.malyavsky@protonmail.com>
2021-04-25 18:45:56 +0300

    added drawing candidates and chooser func

--- includes/inter.h
+++ includes/inter.h
@@ -2,6 +2,16 @@
 # define INTER_H
 
 
+typedef struct s_candidate
+{
+	t_color color;
+	float dist;
+}	t_candidate;
+
+t_candidate *cand_new(t_color color, float dist);
+t_color choose_candidate(t_list **list);
+
 t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene);
 float inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere);
+float inter_plane(t_vec *origin, t_vec *ray, t_plane *plane);
 #endif
---
+++ src/inter/candidate.c
@@ -0,0 +1,39 @@
+#include "minirt.h"
+
+t_candidate *cand_new(t_color color, float dist)
+{
+	t_candidate *result;
+
+	result = malloc(sizeof(t_candidate));
+	if (!result)
+		handle_error(ERR_ALLOC);
+	result->color = color;
+	result->dist = dist;
+	return (result);
+}
+
+t_color choose_candidate(t_list **list)
+{
+	t_list *current;
+	float dist_min;
+	t_candidate *cand;
+	t_candidate *winner;
+
+	dist_min = INFINITY;
+	winner = NULL;
+
+	current = *list;
+	while (current)
+	{
+		cand = current->data;
+		if (cand->dist < dist_min)
+			winner = cand;
+
+		current = current->next;
+	}
+
+	if (winner)
+		return (winner->color);
+	else
+		return (-1);
+}
