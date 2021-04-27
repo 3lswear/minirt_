700b45a6a9d3e244c5262eea53379ba558dc6b41
3lswear <r.malyavsky@protonmail.com>
2021-04-28 01:50:11 +0300

    init

---
+++ src/inter/inter_square.c
@@ -0,0 +1,17 @@
+#include "minirt.h"
+
+float inter_square(t_point *origin, t_vec *ray, t_square *square)
+{
+	float ray_len;
+	
+	t_plane plane;
+	plane.color = BLACK;
+	plane.norm = square->norm;
+	plane.pos = square->pos;
+	ray_len = inter_plane(origin, ray, &plane);
+	if (ray_len < 0)
+		return (ray_len);
+	v_
+
+
+}
