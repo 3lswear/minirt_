e000187e3f18f39a9935a18cf034f81dbcd2cb1f
3lswear <r.malyavsky@protonmail.com>
2021-04-29 00:57:12 +0300

    remade with t_moller struct

--- src/inter/inter_triang.c
+++ src/inter/inter_triang.c
@@ -2,34 +2,26 @@
 
 float	inter_triang(t_point *origin, t_vec *ray, t_triang *tri)
 {
-	t_vec *edge1;
-	t_vec *edge2;
-	t_vec *h;
-	t_vec *s;
-	t_vec *q;
-	float a;
-	float f;
-	float u;
-	float v;
-	float t;
+	t_vec		*edge1;
+	t_vec		*edge2;
+	t_moller	mol;
 
 	edge1 = v_sub(tri->b, tri->a);
 	edge2 = v_sub(tri->c, tri->a);
-	h = v_cross(ray, edge2);
-	a = v_dot_product(edge1, h);
-	if (a > -FLT_EPSILON && a < FLT_EPSILON)
+	mol.pvec = v_cross(ray, edge2);
+	mol.determ = v_dot_product(edge1, mol.pvec);
+	/* if (determ > -FLT_EPSILON && determ < FLT_EPSILON) */
+	/* 	return (0); */
+	if (v_dot_product(tri->norm, ray) == 0)
 		return (0);
-	f = 1.0 / a;
-	s = v_sub(origin, tri->a);
-	u = f * v_dot_product(s, h);
-	if (u < 0.0 || u > 1.0)
+	mol.tvec = v_sub(origin, tri->a);
+	mol.u = v_dot_product(mol.tvec, mol.pvec) / mol.determ;
+	mol.qvec = v_cross(mol.tvec, edge1);
+	mol.v = v_dot_product(ray, mol.qvec) / mol.determ;
+	if (mol.u < 0.0 || mol.u > 1.0 || mol.v < 0.0 || mol.u + mol.v > 1.0)
 		return (0);
-	q = v_cross(s, edge1);
-	v = f * v_dot_product(ray, q);
-	if (v < 0.0 || u + v > 1.0)
-		return (0);
-	t = f * v_dot_product(edge2, q);
-	if (t > FLT_EPSILON)
-		return (v_dot_product(edge2, q) * f);
+	mol.t = v_dot_product(edge2, mol.qvec) / mol.determ;
+	if (mol.t > FLT_EPSILON)
+		return (v_dot_product(edge2, mol.qvec) / mol.determ);
 	return (0);
 }
