ad5359b57cd768d2bb71734af4bc3d7c58a0f543
3lswear <r.malyavsky@protonmail.com>
2021-04-29 00:07:32 +0300

    added moller-trumbore struct, normed

--- includes/inter.h
+++ includes/inter.h
@@ -1,25 +1,37 @@
 #ifndef INTER_H
 # define INTER_H
 
-
 typedef struct s_candidate
 {
-	t_color color;
-	float dist;
+	t_color	color;
+	float	dist;
 }	t_candidate;
 
-t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene);
-float inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere);
-float inter_plane(t_vec *origin, t_vec *ray, t_plane *plane);
+t_color	inter_objects(t_cam *cam, t_vec *ray, t_scene *scene);
+float	inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere);
+float	inter_plane(t_vec *origin, t_vec *ray, t_plane *plane);
 float	inter_triang(t_vec *origin, t_vec *ray, t_triang *tri);
 
-t_candidate *cand_new(t_color color, float dist);
-t_color choose_candidate(t_list **list);
+/* t_candidate	*cand_new(t_color color, float dist); */
+t_color	choose_candidate(t_list **list);
 
-t_color calc_lights(t_vec *norm, t_scene *scene, t_vec *ray, float ray_min);
-t_color calc_lights_2s(t_vec *norm, t_scene *scene, t_vec *ray, float ray_min);
+t_color	calc_lights(t_vec *norm, t_scene *scene, t_vec *ray, float ray_min);
+t_color	calc_lights_2s(t_vec *norm, t_scene *scene, t_vec *ray, float ray_min);
 
-t_color calc_light_matte(t_vec *n, t_light *light, t_vec *ray, float ray_len);
-t_color calc_light_shiny(t_sphere *sphere, t_light *light, t_vec *ray, float ray_len);
+t_color	calc_light_matte(t_vec *n, t_light *light, t_vec *ray, float ray_len);
+t_color	calc_light_shiny(t_sphere *sphere, t_light *light,
+			t_vec *ray, float ray_len);
+
+typedef struct s_moller
+{
+	float	determ;
+	float	u;
+	float	v;
+	float	t;
+	t_vec	*pvec;
+	t_vec	*tvec;
+	t_vec	*qvec;
+
+}	t_moller;
 
 #endif
