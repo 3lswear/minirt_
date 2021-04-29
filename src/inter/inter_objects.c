2c91972e39feba4571f67945ccbfa873e90d4e71
3lswear <r.malyavsky@protonmail.com>
2021-04-30 00:13:22 +0300

    use cam->ray

--- src/inter/inter_objects.c
+++ src/inter/inter_objects.c
@@ -15,6 +15,7 @@ t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 
 	t_hit hit;
 
+	(void)ray;
 	ray_min = INFINITY;
 	closest = NULL;
 	color = -1;
@@ -24,13 +25,13 @@ t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 		cur_obj = current->data;
 		if (cur_obj->type == T_SPHERE)
 		{
-			hit = inter_sphere(cam->origin, ray, &cur_obj->obj.sphere);
+			hit = inter_sphere(cam->origin, cam->ray, &cur_obj->obj.sphere);
 			ray_len = get_positive(hit);
 		}
 		else if (cur_obj->type == T_PLANE)
-			ray_len = inter_plane(cam->origin, ray, &cur_obj->obj.plane);
+			ray_len = inter_plane(cam->origin, cam->ray, &cur_obj->obj.plane);
 		else if (cur_obj->type == T_TRIANG)
-			ray_len = inter_triang(cam->origin, ray, &cur_obj->obj.triang);
+			ray_len = inter_triang(cam->origin, cam->ray, &cur_obj->obj.triang);
 		if ((ray_len > 0) && (ray_len < ray_min))
 		{
 			ray_min = ray_len;
@@ -46,7 +47,7 @@ t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 	{
 		if (closest->type == T_SPHERE)
 		{
-			surface_point = v_mult(ray, ray_min);
+			surface_point = v_mult(cam->ray, ray_min);
 			surface_point = v_add(surface_point, cam->origin);
 			/* print_vec(surface_point, "surface_point"); */
 			norm = v_sub(surface_point, closest->obj.sphere.center);
@@ -55,7 +56,7 @@ t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 			if (scene->lights)
 				color = c_mul(closest->obj.sphere.color,
 						c_add(ambient,
-							calc_lights_2s(norm, scene, ray, ray_min, cam)));
+							calc_lights_2s(norm, scene, cam->ray, ray_min, cam)));
 			else
 				color = c_mul(ambient, closest->obj.sphere.color);
 			free(norm);
@@ -66,7 +67,7 @@ t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 			if (scene->lights)
 				color = c_mul(closest->obj.plane.color,
 						c_add(ambient,
-							calc_lights_2s(closest->obj.plane.norm, scene, ray, ray_min, cam)));
+							calc_lights_2s(closest->obj.plane.norm, scene, cam->ray, ray_min, cam)));
 			else
 				color = c_mul(ambient, closest->obj.plane.color);
 		}
@@ -75,7 +76,7 @@ t_color inter_objects(t_cam *cam, t_vec *ray, t_scene *scene)
 			if (scene->lights)
 				color = c_mul(closest->obj.triang.color,
 						c_add(ambient,
-							calc_lights_2s(closest->obj.triang.norm, scene, ray, ray_min, cam)));
+							calc_lights_2s(closest->obj.triang.norm, scene, cam->ray, ray_min, cam)));
 			else
 				color = c_mul(ambient, closest->obj.triang.color);
 		}
