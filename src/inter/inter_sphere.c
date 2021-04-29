b4eae3ef126349383bd214dcf3cf9c7b277a9a82
3lswear <r.malyavsky@protonmail.com>
2021-04-30 00:13:35 +0300

    removed comments

--- src/inter/inter_sphere.c
+++ src/inter/inter_sphere.c
@@ -5,14 +5,8 @@ t_hit inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere)
 	double b;
 	double c;
 	double discr;
-	/* double hit1; */
-	/* double hit2; */
 	t_vec *cam2sphere;
 	t_hit hit;
-
-	/* hit1 = 0; */
-	/* hit2 = 0; */
-
 
 	/* printf("sphere->radius = %f\n", sphere->radius); */
 	cam2sphere = v_sub(origin, sphere->center);
@@ -29,50 +23,4 @@ t_hit inter_sphere(t_vec *origin, t_vec *ray, t_sphere *sphere)
 	hit.a = (-b - sqrt(discr)) / 2;
 	hit.b = (-b + sqrt(discr)) / 2;
 	return (hit);
-	//choose which distance to return
-	/* if ((hit1 * hit2) > 0) */
-	/* ОПТИМИЗИРОВАТЬ */
-
-	/* if (hit1 > 0 && hit2 > 0) */
-	/* { */
-	/* 	if (hit1 > hit2) */
-	/* 		return (hit2); */
-	/* 	else */
-	/* 		return (hit1); */
-	/* } */
-	/* else if ((hit1 * hit2) < 0) */
-	/* { */
-	/* 	/1* if (hit1 > 0) *1/ */
-	/* 	/1* 	return (hit1); *1/ */
-	/* 	/1* else *1/ */
-	/* 	/1* 	return (hit2); *1/ */
-	/* 	if (hit1 > hit2) */
-	/* 		return (hit1); */
-	/* 	else */
-	/* 		return (hit2); */
-	/* } */
-	/* else */
-	/* 	return (0); */
-
-	/* /1* if both positive then return smaller one *1/ */
-	/* if ((hit1 * hit2 > 0) && hit1 > FLT_EPSILON) */
-	/* { */
-	/* 	if (hit1 > hit2) */
-	/* 		return (hit2); */
-	/* 	else */ 
-	/* 		return (hit1); */
-	/* } */
-	/* else if ((hit1 * hit2) < 0) */
-	/* { */
-	/* 	if (hit1 > hit2) */
-	/* 		return (hit1); */
-	/* 	else */ 
-	/* 		return (hit2); */
-	/* } */
-	/* /1* else if (hit1 > FLT_EPSILON) *1/ */
-	/* /1* 	return (hit1); *1/ */
-	/* /1* else if (hit2 > FLT_EPSILON) *1/ */
-	/* /1* 	return (hit2); *1/ */
-	/* else */
-	/* 	return (0); */
 }
