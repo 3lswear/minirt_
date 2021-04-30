ca33970771663c083728c673357945eac59a3c2f
3lswear <r.malyavsky@protonmail.com>
2021-04-30 04:17:32 +0300

    updated new_plane

--- src/fig/plane.c
+++ src/fig/plane.c
@@ -1,10 +1,11 @@
 #include "minirt.h"
 
-t_plane new_plane(t_point *pos, t_vec *norm)
+t_plane new_plane(t_point *pos, t_vec *norm, t_color color)
 {
 	t_plane result;
 
 	result.pos = pos;
 	result.norm = norm;
+	result.color = color;
 	return (result);
 }
