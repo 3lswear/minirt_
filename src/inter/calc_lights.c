843363c76e73c96c3d4a5f156efb8e33063b71b9
3lswear <r.malyavsky@protonmail.com>
2021-05-02 18:25:29 +0300

    added cylind to shadows

--- src/inter/calc_lights.c
+++ src/inter/calc_lights.c
@@ -22,6 +22,8 @@ static	int just_intersect(t_point *start, t_vec *vec, t_list *objects, double ma
 			ray_len = inter_triang(start, vec, &cur_obj->obj.triang);
 		else if (cur_obj->type == T_SQUARE)
 			ray_len = inter_square(start, vec, &cur_obj->obj.square);
+		else if (cur_obj->type == T_CYLIND)
+			ray_len = just_get(inter_cylind(start, vec, &cur_obj->obj.cylind));
 		/* if ((ray_len < max_len) && (ray_len > max_len * FLT_EPSILON * 100)) */
 		if ((ray_len < max_len) && (ray_len > 0))
 		{
@@ -86,7 +88,7 @@ t_color	calc_lights_2s(t_vec *norm, t_scene *scene, double ray_min, t_cam *cam)
 				calc_light_matte(norm_mod, light, cam->ray, ray_min, cam));
 
 		/* result = c_add(result, */
-		/* 	calc_light_matte(norm_mod, light, ray, ray_min, cam)); */
+		/* 	calc_light_matte(norm_mod, light, cam->ray, ray_min, cam)); */
 		free(norm_mod);
 		current = current->next;
 	}
