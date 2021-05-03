227e6b4150009fc90ce747164b3191efc7efbacd
3lswear <r.malyavsky@protonmail.com>
2021-05-03 18:08:29 +0300

    added res check

--- src/parse/parse_res.c
+++ src/parse/parse_res.c
@@ -1,8 +1,12 @@
 #include "minirt.h"
 
-void	parse_res(char **line, t_scene *scene)
+void	parse_res(char **line, t_scene *scene, t_win *win)
 {
+	(void)win;
 	scene->width = ft_atoi(line[1]);
 	scene->height = ft_atoi(line[2]);
-
+	if (scene->width > W_MAX)
+		scene->width = W_MAX;
+	if (scene->height > H_MAX)
+		scene->height = H_MAX;
 }
