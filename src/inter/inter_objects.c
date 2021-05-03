dec79df8d41a7a06a301cce9131182b58714c9a3
3lswear <r.malyavsky@protonmail.com>
2021-05-03 16:38:46 +0300

    compacted even more

--- src/inter/inter_objects.c
+++ src/inter/inter_objects.c
@@ -1,45 +1,64 @@
 #include "minirt.h"
 
-t_color	color_closest(t_scene *scene, t_cam *cam, t_obj *closest, t_color ambient, double ray_min)
+t_color	return_sph_color(t_scene *scene, t_cam *cam, t_obj *closest,
+			double ray_min)
 {
-	t_point	*surface_point;
+	t_color	color;
 	t_vec	*norm;
+	t_point	*surface_point;
+	t_color	ambient;
+
+	ambient = c_mul_scalar(scene->ambient, scene->amb_intensity);
+	surface_point = v_mult(cam->ray, ray_min);
+	v_add_inplace(surface_point, cam->origin);
+	norm = v_sub(surface_point, closest->obj.sphere.center);
+	v_norm_inplace(norm);
+	color = c_mul(closest->color, c_add(ambient, calc_lights_2s
+				(norm, scene, ray_min, cam)));
+	free(norm);
+	free(surface_point);
+	return (color);
+}
+
+t_color	return_cy_color(t_scene *scene, t_cam *cam, t_obj *cl, double ray_min)
+{
+	t_color	ambient;
 	t_color	color;
 
-	/* color = new_color(-1, -1, -1); */
+	ambient = c_mul_scalar(scene->ambient, scene->amb_intensity);
+	color = c_mul(cl->color, c_add(ambient, calc_lights_2s
+				(cl->obj.cylind.cur_norm, scene, ray_min, cam)));
+	free(cl->obj.cylind.cur_norm);
+	return (color);
+}
+
+t_color	color_closest(t_scene *scene, t_cam *cam, t_obj *cl,
+			double ray_min)
+{
+	t_color	color;
+	t_color	ambient;
+
+	ambient = c_mul_scalar(scene->ambient, scene->amb_intensity);
 	if (scene->lights)
 	{
-		if (closest->type == T_SPHERE)
-		{
-			surface_point = v_mult(cam->ray, ray_min);
-			v_add_inplace(surface_point, cam->origin);
-			norm = v_sub(surface_point, closest->obj.sphere.center);
-			v_norm_inplace(norm);
-			color = c_mul(closest->color, c_add(ambient,
-						calc_lights_2s(norm, scene, ray_min, cam)));
-			free(norm);
-			free(surface_point);
-		}
-		else if (closest->type == T_PLANE)
-			color = c_mul(closest->color, c_add(ambient, calc_lights_2s
-						(closest->obj.plane.norm, scene, ray_min, cam)));
-		else if (closest->type == T_TRIANG)
-			color = c_mul(closest->color, c_add(ambient, calc_lights_2s
-						(closest->obj.triang.norm, scene, ray_min, cam)));
-		else if (closest->type == T_SQUARE)
-			color = c_mul(closest->color, c_add(ambient, calc_lights_2s
-						(closest->obj.square.norm, scene, ray_min, cam)));
-		else if (closest->type == T_CYLIND)
-		{
-			color = c_mul(closest->color, c_add(ambient, calc_lights_2s
-						(closest->obj.cylind.cur_norm, scene, ray_min, cam)));
-			free(closest->obj.cylind.cur_norm);
-		}
+		if (cl->type == T_SPHERE)
+			color = return_sph_color(scene, cam, cl, ray_min);
+		else if (cl->type == T_PLANE)
+			color = c_mul(cl->color, c_add(ambient, calc_lights_2s
+						(cl->obj.plane.norm, scene, ray_min, cam)));
+		else if (cl->type == T_TRIANG)
+			color = c_mul(cl->color, c_add(ambient, calc_lights_2s
+						(cl->obj.triang.norm, scene, ray_min, cam)));
+		else if (cl->type == T_SQUARE)
+			color = c_mul(cl->color, c_add(ambient, calc_lights_2s
+						(cl->obj.square.norm, scene, ray_min, cam)));
+		else if (cl->type == T_CYLIND)
+			color = return_cy_color(scene, cam, cl, ray_min);
 		else
-			color = c_mul(ambient, closest->color);
+			color = c_mul(ambient, cl->color);
 	}
 	else
-		color = c_mul(ambient, closest->color);
+		color = c_mul(ambient, cl->color);
 	return (color);
 }
 
@@ -79,7 +98,7 @@ t_color	inter_objects(t_cam *cam, t_scene *scene)
 	}
 	ambient = c_mul_scalar(scene->ambient, scene->amb_intensity);
 	if (closest)
-		return (color_closest(scene, cam, closest, ambient, ray_min));
+		return (color_closest(scene, cam, closest, ray_min));
 	else
 		return (new_color(-1, -1, -1));
 }
